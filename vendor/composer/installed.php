c5fdbc5b7586ecdcaf8681462c1b6cb8c3e48ff6
Jamelich <serj-post@mail.ru>
2022-07-14 02:49:54 +0300

    Новый проект

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'electrolinux/phpquery' => array(
+            'pretty_version' => '0.9.6',
+            'version' => '0.9.6.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../electrolinux/phpquery',
+            'aliases' => array(),
+            'reference' => '6cb8afcfe8cd4ce45f2f8c27d561383037c27a3a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
